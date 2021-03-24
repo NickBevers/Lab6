bcfd98553676ed1d4944254b64cf7a42e0215a1f
nickbevers <nick.bevers9@gmail.com>
2021-03-24 21:32:20 +0100

    fix: Add live.pug and index.pug to views folder; Changerouter/index.js logic

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('esports/index', {});
+  res.render('index', {});
 });
 
 router.get('/live', function(req, res, next) {
-  res.render('esports/live', {});
+  res.render('live', {});
 });
 
 module.exports = router;
