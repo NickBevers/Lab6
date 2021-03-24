8a8c5d8bfe1623e441520b1c22f5eccc12c92865
nickbevers <nick.bevers9@gmail.com>
2021-03-24 20:34:19 +0100

    fix: Add connection between server and sparks; Add data to write

--- primus/live.js
+++ primus/live.js
@@ -2,6 +2,16 @@ const Primus = require("primus");
 
 function go(server){
     primus = new Primus(server, {});
+
+    primus.on('connection', (spark) => {
+        console.log("New spark");
+
+        
+        spark.on("data", (data) => {
+            primus.write(data);
+        })
+      });
+      
 }
 
 module.exports.go = go;
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,18 @@
+const Primus = require("primus");
+
+let primus = Primus.connect("/", {
+    reconnect: {
+        max: Infinity // Number: The max delay before we try to reconnect.
+      , min: 500 // Number: The minimum delay before we try reconnect.
+      , retries: 10 // Number: How many times we should try to reconnect.
+    }
+});
+
 document.querySelector(".teams").addEventListener("click", (e) =>{
     let favoTeam = e.target.parentElement.getAttribute("class");
     console.log(favoTeam);
+
+    primus.write({
+        team: favoTeam
+    })
 })
